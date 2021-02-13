faa41b47a5df90968b021c4f238bf29f5ab4a471
DIERICK Dewi <dierickd@outlook.fr>
2021-02-13 19:40:51 +0100

    modify title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-premier-projet';
+  title = 'Bienvenue sur le site de Déwi !';
 }
